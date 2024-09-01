c83b32b895d8306e27258dde576af978af84d032
Karan Dixit <karandixit10@gmail.com>
2024-09-02 03:25:01 +0530

    Respond to multiple PINGs

--- src/Server.cpp
+++ src/Server.cpp
@@ -8,6 +8,8 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+#define BUFFER_SIZE 1024
+
 int main(int argc, char **argv)
 {
   // Flush after every std::cout / std::cerr
@@ -49,17 +51,40 @@ int main(int argc, char **argv)
   {
     std::cerr << "listen failed\n";
     return 1;
-  } 
+  }
 
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
 
   std::cout << "Waiting for a client to connect...\n";
 
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  send(client_fd, "+PONG\r\n", 7, 0);
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+  if (client_fd < 0)
+  {
+    std::cerr << "Failed to accept client connection\n";
+    return 1;
+  }
   std::cout << "Client connected\n";
 
+  char buffer[BUFFER_SIZE];
+  const char *response = "+PONG\r\n";
+
+  while (true)
+  {
+    ssize_t bytes_read = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_read <= 0)
+    {
+      // Connection closed or error occurred
+      break;
+    }
+    buffer[bytes_read] = '\0';
+    std::cout << "Received: " << buffer;
+
+    // For now, we're responding with PONG regardless of the input
+    send(client_fd, response, strlen(response), 0);
+  }
+
+  close(client_fd);
   close(server_fd);
 
   return 0;
