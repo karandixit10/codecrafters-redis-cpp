8b10d796bf5c432449908b54917f2c14cf3cc3d7
Karan Dixit <karandixit10@gmail.com>
2024-09-03 00:08:00 +0530

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,9 +7,33 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <thread>
 
 #define BUFFER_SIZE 1024
 
+void handle_client(int client_fd)
+{
+  char buffer[BUFFER_SIZE];
+  const char *response = "+PONG\r\n";
+
+  while (true)
+  {
+    ssize_t bytes_read = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_read <= 0)
+    {
+      // Connection closed or error occurred
+      break;
+    }
+    buffer[bytes_read] = '\0';
+    std::cout << "Received: " << buffer;
+
+    // For now, we're responding with PONG regardless of the input
+    send(client_fd, response, strlen(response), 0);
+  }
+
+  close(client_fd);
+  std::cout << "Client disconnected\n";
+}
 int main(int argc, char **argv)
 {
   // Flush after every std::cout / std::cerr
@@ -53,38 +77,26 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-
   std::cout << "Waiting for a client to connect...\n";
-
-  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-  if (client_fd < 0)
-  {
-    std::cerr << "Failed to accept client connection\n";
-    return 1;
-  }
-  std::cout << "Client connected\n";
-
-  char buffer[BUFFER_SIZE];
-  const char *response = "+PONG\r\n";
 
   while (true)
   {
-    ssize_t bytes_read = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
-    if (bytes_read <= 0)
-    {
-      // Connection closed or error occurred
-      break;
-    }
-    buffer[bytes_read] = '\0';
-    std::cout << "Received: " << buffer;
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
 
-    // For now, we're responding with PONG regardless of the input
-    send(client_fd, response, strlen(response), 0);
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+    if (client_fd < 0)
+    {
+      std::cerr << "Failed to accept client connection\n";
+      continue;
+    }
+    std::cout << "Client connected\n";
+
+    // Create a new thread to handle the client
+    std::thread client_thread(handle_client, client_fd);
+    client_thread.detach(); // Detach the thread to allow it to run independently
   }
 
-  close(client_fd);
   close(server_fd);
 
   return 0;
