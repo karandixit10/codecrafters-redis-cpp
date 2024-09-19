f4bbaf902fca94fbc44ff0a9877453f92b137e66
Karan Dixit <karandixit10@gmail.com>
2024-09-19 14:46:41 +0530

    codecrafters submit [skip ci]

--- src/ClientHandler.hpp
+++ src/ClientHandler.hpp
@@ -3,23 +3,31 @@
 
 #include <string>
 #include <map>
+#include <vector>
+#include <mutex>
 #include "RedisParser.hpp"
 
 class ClientHandler {
 public:
     ClientHandler();
     void handleClient(int clientSocket);
+    static void setInitialConfig(const std::string& initialDir, const std::string& initialDbfilename);
 
 private:
-    std::map<std::string, std::string> keyValues;
-    std::map<std::string, long> expTime;
-    RedisParser parser;
-
     void handleSet(const std::vector<std::string>& command, int clientSocket);
     void handleGet(const std::vector<std::string>& command, int clientSocket);
     void handleEcho(const std::vector<std::string>& command, int clientSocket);
     void handlePing(int clientSocket);
+    void handleConfig(const std::vector<std::string>& command, int clientSocket);
     void sendResponse(int clientSocket, const std::string& response);
+    void handleConfigGet(const std::string& parameter, int clientSocket);
+
+    RedisParser parser;
+    static std::map<std::string, std::string> keyValues;
+    static std::map<std::string, long> expTime;
+    static std::mutex mapMutex;
+    static std::string dir;
+    static std::string dbfilename;
 };
 
 #endif // CLIENT_HANDLER_HPP
